9393bcb39f0291cf3a3dc6faa9d0dec5f7d86208
Minh Vu <choquayly123@gmail.com>
2025-12-12 07:41:13 +0700

    e vu an cut

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace QLKS_Winform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new login());
         }
     }
 }
