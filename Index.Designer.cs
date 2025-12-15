cac092320f20894212b87b4dc53ed9128caedf53
Minh Vu <choquayly123@gmail.com>
2025-12-15 23:40:59 +0700

    x

--- Index.Designer.cs
+++ Index.Designer.cs
@@ -28,22 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.plMenu = new System.Windows.Forms.Panel();
+            this.plData = new System.Windows.Forms.Panel();
+            this.plLogo = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.plMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // plMenu
+            // 
+            this.plMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(27)))), ((int)(((byte)(53)))));
+            this.plMenu.Controls.Add(this.button1);
+            this.plMenu.Controls.Add(this.plLogo);
+            this.plMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.plMenu.Location = new System.Drawing.Point(0, 0);
+            this.plMenu.Name = "plMenu";
+            this.plMenu.Size = new System.Drawing.Size(218, 681);
+            this.plMenu.TabIndex = 0;
+            // 
+            // plData
+            // 
+            this.plData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.plData.Dock = System.Windows.Forms.DockStyle.Right;
+            this.plData.Location = new System.Drawing.Point(214, 0);
+            this.plData.Name = "plData";
+            this.plData.Size = new System.Drawing.Size(1050, 681);
+            this.plData.TabIndex = 1;
+            // 
+            // plLogo
+            // 
+            this.plLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(27)))), ((int)(((byte)(53)))));
+            this.plLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.plLogo.Location = new System.Drawing.Point(0, 0);
+            this.plLogo.Name = "plLogo";
+            this.plLogo.Size = new System.Drawing.Size(218, 100);
+            this.plLogo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Index
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.plData);
+            this.Controls.Add(this.plMenu);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Index";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Index";
+            this.plMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel plMenu;
+        private System.Windows.Forms.Panel plData;
+        private System.Windows.Forms.Panel plLogo;
+        private System.Windows.Forms.Button button1;
     }
 }
