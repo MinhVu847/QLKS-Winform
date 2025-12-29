d0da5fbf27084667916547dc6368d948e0b6590c
Minh Vu <choquayly123@gmail.com>
2025-12-29 20:36:09 +0700

    x

--- login.Designer.cs
+++ login.Designer.cs
@@ -172,9 +172,9 @@
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
             this.label2.Location = new System.Drawing.Point(43, 95);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(250, 17);
+            this.label2.Size = new System.Drawing.Size(256, 17);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Welcome Back ! Please login your Acount.";
+            this.label2.Text = "Welcome Back ! Please login your Account.";
             // 
             // label1
             // 
